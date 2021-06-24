22c7201655f16418fe9568f2a096b31629a1c7b4
EugeniySichev <sichev91@inbox.ru>
2021-06-24 20:25:27 +0300

    added abbility to add images

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.forms import ModelChoiceForm
 
 from django import forms
 
@@ -6,29 +7,21 @@ from .models import *
 
 
 
-
-
-class NotebookCategoryChoiceField(forms.ModelChoiceField):
-
-    pass
-
 class NotebookAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug='notebooks'))
+            return ModelChoiceForm(Category.objects.filter(slug='notebooks'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
-class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
 
-    pass
 
 class SmartphoneAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug='smartphones'))
+            return ModelChoiceForm(Category.objects.filter(slug='smartphones'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
