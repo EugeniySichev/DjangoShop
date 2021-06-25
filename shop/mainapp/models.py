cc0d9e1ad0ebc39c26d747fd2b33d66d335858de
EugeniySichev <sichev91@inbox.ru>
2021-06-25 16:21:05 +0300

    added model-view-template

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -6,10 +6,17 @@ from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.urls import reverse
 
 from io import BytesIO
 
 User = get_user_model()
+
+
+def get_product_url(obj, viewname, model_name):
+    ct_model = obj.__class__.meta.model_name
+    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
+
 
 
 class MinResolutionErrorException(Exception):
@@ -112,6 +119,9 @@ class Notebook(Product):
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
 
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
+
 
 
 class Smartphone(Product):
@@ -128,6 +138,9 @@ class Smartphone(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class CartProduct(models.Model):
